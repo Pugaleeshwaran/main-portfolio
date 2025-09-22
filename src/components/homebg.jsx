9138a4c1b7755208f70fc6e1c376df1cc1ed6fe3
Pugaleeshwaran <pugalpugal306@gmail.com>
2025-09-23 00:01:52 +0530

    add-type-animation

--- src/components/homebg.jsx
+++ src/components/homebg.jsx
@@ -1,8 +1,28 @@
 import Bghome from "../assetes/coding.jpg"
-const Homebg=()=>{
-    return(
+import { useTypewriter, Cursor } from "react-simple-typewriter"
+const Homebg = () => {
+    const [text] = useTypewriter({
+        words: ["I am Pugaleeshwaran", "MERN Stack Developer",
+            "Full Stack Developer", "Grapic Designer", "Professional Editor", "Java"],
+        loop: 0,
+        typeSpeed: 120,
+        deleteSpeed: 80,
+    })
+    return (
         <div className="homebg-container">
-            <h3>Pugaleeshwaran</h3>
+            <h3>
+                Welcome
+            </h3>
+            <h4 className="type-text">
+                {text}
+                <span className="type-text-pin">
+                    <Cursor />
+                </span>
+                </h4>
+                <p>
+                    Based in Chennai, Tamil Nadu, India.
+                </p>
+                <button>Hire Me</button>
         </div>
     )
 }
