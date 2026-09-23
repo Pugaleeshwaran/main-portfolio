17c56b2f01d019c1fa3fa120f4766e0d3a81307e
Pugaleeshwaran <pugalpugal306@gmail.com>
2026-09-23 20:33:20 +0530

    upadate resume

--- src/components/projectlist.jsx
+++ src/components/projectlist.jsx
@@ -14,11 +14,13 @@ const Projectlist = () => {
     return (
         <div className="project-list">
             <Allproject img={home} h3={"Amazon Prime Clone"} p={"A fully responsive Amazon Prime Clone Website built using HTML, CSS, and JavaScript."} link={"https://pugaleeshwaran.github.io/Amazon_prime_clone/"} />
-            <Allproject img={act} h3={"Act-Doto"} p={"Developed a responsive To-Do List web application with user-friendly interface and task management features."} link={"https://actdoto-react.vercel.app/"} />
             <Allproject img={cfc} h3={"TFC Nation"} p={"Fully Responsive Webside UI build using HTML,CSS for Restaurants."} link={"https://pugaleeshwaran.github.io/TFC-web-project/"} />
-            <Allproject img={udemy} h3={"Udemy"} p={"built a Udemy Clone Website using HTML and CSS and React js and it’s fully responsive across all devices!"} link={"https://udemy-clone-react-apab.vercel.app/"} />
+             <Allproject img={udemy} h3={"Udemy"} p={"built a Udemy Clone Website using HTML and CSS and React js and it’s fully responsive across all devices!"} link={"https://udemy-clone-react-apab.vercel.app/"} />
             <Allproject img={trip} h3={"Trip Adviser"} p={"a clean and modern frontend experience inspired by Trip adviser Clone."} link={"https://pugaleeshwaran.github.io/Trip_Advisor-Clone/"} />
             <Allproject img={menstra} h3={"Menstra"} p={"Menstra, an e-commerce website built using HTML, CSS, and JavaScript. This platform is designed to be fully responsive across all devices."} link={"https://pugaleeshwaran.github.io/menstra/"} />
+            <Allproject img={act} h3={"Act-Doto"} p={"Developed a responsive To-Do List web application with user-friendly interface and task management features."} link={"https://actdoto-react.vercel.app/"} />
+
+           
             <Allproject img={weather} h3={"Weather Report app"} p={"Developed a weather application using JavaScript, HTML, CSS, and OpenWeather API Implemented real-time weather data fetching based on user input "} link={"https://weather-app-2qmb.vercel.app/"} />
             <Allproject img={green} h3={"Greendan"} p={"a fully responsive website built using HTML and Tailwind CSS and Java Script"} link={"https://pugaleeshwaran.github.io/greendon-webpage/"} />
             <Allproject img={perfumy} h3={"Perfumy"} p={"a fully responsive website built using HTML and css React Js"} link={"https://perfumy-react-lphd.vercel.app/"} />
